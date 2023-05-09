038d4366abd57bf73bf42ebf9a0f800b2090ca53
jfrasinska <j.frasinska@gmail.com>
2023-05-09 23:13:34 +0200

    adding second task script

---
+++ src/js/02-timer.js
@@ -0,0 +1,63 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+
+let countdownInterval;
+
+function countdown() {
+  const eventDate = new Date(datetimePicker.value).getTime();
+  const currentDate = new Date().getTime();
+  const timeDifference = eventDate - currentDate;
+
+  if (timeDifference < 0) {
+    clearInterval(countdownInterval);
+    window.alert('Please choose a date in the future');
+    return;
+  }
+
+  let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  days = days < 10 ? '0' + days : days;
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  daysSpan.textContent = days;
+  hoursSpan.textContent = hours;
+  minutesSpan.textContent = minutes;
+  secondsSpan.textContent = seconds;
+}
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate < currentDate) {
+      window.alert('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+});
+
+startButton.addEventListener('click', () => {
+  countdown();
+  countdownInterval = setInterval(countdown, 1000);
+});
